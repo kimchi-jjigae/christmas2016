ae5258eff0f471ed1467040795748d9ab5fa23fa
Kim Wong <kimspindel@gmail.com>
2016-08-12 09:12:46 +0200

    simple jumping and movement physics

--- site/js/game.js
+++ site/js/game.js
@@ -6,6 +6,13 @@ var game = new Phaser.Game(1366, 768, Phaser.AUTO, '', {
     update:  update
 });
 
+var santaValues = {
+    speed: 300,
+    jump: 600,
+    gravity: 1000,
+    bounce: 0.2,
+}
+
 function preload() {
     game.load.image('mg',       'assets/sprites/mg.png');
     game.load.image('santa',    'assets/sprites/santa.png');
@@ -15,11 +22,18 @@ function preload() {
 }
 
 var platforms;
+var santa;
+var cursors;
 
 function create() {
+    cursors = game.input.keyboard.createCursorKeys();
     game.stage.backgroundColor = "#ddffdd";
-    var santa = game.add.sprite(0, 0, 'santa');
+    santa = game.add.sprite(0, 0, 'santa');
     santa.scale.setTo(0.2, 0.2);
+    game.physics.arcade.enable(santa);
+    santa.body.bounce.y = santaValues.bounce;
+    santa.body.gravity.y = santaValues.gravity;
+    santa.body.collideWorldBounds = true;
 
     platforms = game.add.group();
     platforms.enableBody = true;
@@ -33,4 +47,23 @@ function create() {
 }
 
 function update() {
+    game.physics.arcade.collide(santa, platforms);
+
+    santa.body.velocity.x = 0;
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        santa.body.velocity.x = -santaValues.speed;
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        santa.body.velocity.x = santaValues.speed;
+    }
+
+    //  Allow the player to jump if they are touching the ground.
+    if (cursors.up.isDown && santa.body.touching.down)
+    {
+        santa.body.velocity.y = -santaValues.jump;
+    }
 }
