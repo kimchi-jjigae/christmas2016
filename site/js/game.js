e340de2a20ddfbad9f1690112a73c3b62f5814f5
Kim Wong <kimspindel@gmail.com>
2016-08-12 21:08:31 +0200

    Bullets can now be fired (although much too quickly and too often); keyboard input requires fixing to work with WASD

--- site/js/game.js
+++ site/js/game.js
@@ -18,18 +18,23 @@ function preload() {
     game.load.image('santa',    'assets/sprites/santa.png');
     game.load.image('girl',     'assets/sprites/girl.png');
     game.load.image('platform', 'assets/sprites/platform.png');
+    game.load.image('bullet',   'assets/sprites/bullet.png');
 
 }
 
+var santa;
+var children;
+var mg;
+var bullets;
 var platforms;
 var presents;
-var santa;
+
 var cursors;
-var mg;
-var children;
 
 function create() {
     cursors = game.input.keyboard.createCursorKeys();
+    game.input.keyboard.addKey(Phaser.Keyboard.A);
+
     game.stage.backgroundColor = "#ddffdd";
     santa = game.add.sprite(0, 0, 'santa');
     santa.scale.setTo(0.2, 0.2);
@@ -51,18 +56,39 @@ function create() {
     var present1 = presents.create(0, 64, 'platform');
     present1.scale.setTo(0.1, 1);
 
-    mg = game.add.sprite(680, 0, 'mg');
+    mg = game.add.sprite(680, 100, 'mg');
     mg.scale.setTo(0.5, 0.5);
+    mg.anchor.setTo(0.5, 0.5);
 
     children = game.add.group();
     var girl = children.create(1000, 600, 'girl');
     girl.scale.setTo(0.2, 0.2);
+    game.physics.arcade.enable(children);
+    girl.body.velocity.x = -50;
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+    game.input.keyboard.onDownCallback = function(e) {
+        if(e.key == 'd') {
+            santa.body.velocity.x = santaValues.speed;
+        }
+    }
 }
 
 function update() {
     game.physics.arcade.collide(santa, platforms);
 
     santa.body.velocity.x = 0;
+    mg.rotation = game.physics.arcade.angleToPointer(mg);
+
+    if (game.input.activePointer.isDown)
+    {
+        var bullet = bullets.create(680, 100, 'bullet');
+        game.physics.arcade.moveToPointer(bullet, 300);
+    }
+
     if (cursors.left.isDown)
     {
         //  Move to the left
@@ -77,6 +103,7 @@ function update() {
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && santa.body.touching.down)
     {
+        //santa.kill();
         santa.body.velocity.y = -santaValues.jump;
     }
 }
